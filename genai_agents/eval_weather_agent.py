711a459788ef3b4ec89bf1205c07e95dadaf85f4
V Lakshmanan <lak@vlakshman.com>
2024-11-22 22:58:44 +0000

    Working evaluation

--- genai_agents/eval_weather_agent.py
+++ genai_agents/eval_weather_agent.py
@@ -2,8 +2,9 @@ from unittest.mock import patch, Mock
 import json
 
 from deepeval import evaluate
-from deepeval.metrics.ragas import RagasMetric
 from deepeval.test_case import LLMTestCase
+from deepeval.metrics import GEval
+from deepeval.test_case import LLMTestCaseParams
 
 import lg_weather_agent
 
@@ -35,25 +36,63 @@ def run_query_with_mock():
 
 #run_query_with_mock()
 
+# Set up correctness metric
+llm_as_judge = GEval(
+    name="Correctness",
+    criteria="Determine whether the actual output is factually correct based on the expected output.",
+    evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
+    model='gpt-3.5-turbo'
+)
+
 
 @patch('lg_weather_agent.retrieve_weather_data', Mock(return_value=chicago_weather))
-def test_query_rain_today():
+def eval_query_rain_today():
     input_query = "Is it raining in Chicago?"
     expected_output = "No, it is not raining in Chicago right now."
-    result = lg_weather_agent.run_query(app, "Is it raining in Chicago?")
+    result = lg_weather_agent.run_query(app, input_query)
     actual_output = result[-1]
-    retrieval_context = json.dumps(weather_data)
     
-    # use RAGAS
-    metric = RagasMetric(threshold=0.5, model='gemini-1.5-flash')
+    # metric = ContextualRecallMetric(threshold=0.5, model='gpt-3.5-turbo', include_reason=False)
+    print(f"Actual: {actual_output}   Expected: {expected_output}")
     test_case = LLMTestCase(
         input=input_query,
         actual_output=actual_output,
-        expected_output=expected_output,
-        retrieval_context=retrieval_context
+        expected_output=expected_output
     )
 
-    metric.measure(test_case)
-    print(metric.score)
+    llm_as_judge.measure(test_case)
+    print(llm_as_judge.score)
 
-test_query_rain_today()
+# eval_query_rain_today()
+
+
+@patch('lg_weather_agent.retrieve_weather_data', Mock(return_value=chicago_weather))
+def eval_dataset():
+    # normally, you'll read this from a file
+    dataset = [
+        {
+            "input_query": "Is it raining in Chicago?",
+            "expected_output": "No, it is not raining in Chicago right now."
+        },
+        {
+            "input_query": "How cold is it in Chicago?",
+            "expected_output": "It's around 45F, but winds of 15-25mph makes it feel colder."
+        },        
+    ]
+    
+    for data in dataset:
+        result = lg_weather_agent.run_query(app, data['input_query'])
+        actual_output = result[-1]
+
+        print(f"Actual: {actual_output}   Expected: {data['expected_output']}")
+        test_case = LLMTestCase(
+            input=data['input_query'],
+            actual_output=actual_output,
+            expected_output=data['expected_output']
+        )
+        
+        llm_as_judge.measure(test_case)
+    
+    print(llm_as_judge.score)
+    
+eval_dataset()
