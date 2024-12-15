0b6cf48a2966e1545e51504c8bd3b9838efd550d
V Lakshmanan <lak@vlakshman.com>
2024-12-14 18:06:17 -0800

    Improve output by using a system prompt to specify units

--- rag_pydantic_ai/zero_shot_structured.py
+++ rag_pydantic_ai/zero_shot_structured.py
@@ -1,19 +1,31 @@
 import llm_utils
 from pydantic_ai import Agent
 from dataclasses import dataclass
-from pydantic import Field
+
 
 @dataclass
 class Mountain:
     name: str
-    height: float # in meters
-    taller_than_mt_rainier: bool
-
+    location: str
+    height: float
 
 if __name__ == '__main__':
-    agent = Agent(llm_utils.default_model(), result_type=Mountain)
-    question = "What is the tallest mountain in British Columbia? How tall is it, in meters? Is it taller than Mt. Rainier?"
-    print(">> ", question)
-    answer1 = agent.run_sync(question)
-    print(answer1.data)
+    agent = Agent(llm_utils.default_model(),
+                  result_type=Mountain,
+                  system_prompt=(
+                      "You are a mountaineering guide, who provides accurate information to the general public.",
+                      "Provide all distances and heights in meters",
+                      "Provide location as distance and direction from nearest big city",
+                  ))
+
+    questions = [
+        "Tell me about the tallest mountain in British Columbia?",
+        "Is Mt. Hood easy to climb?",
+        "What's the tallest peak in the Enchantments?"
+    ]
+
+    for question in questions:
+        print(">> ", question)
+        answer = agent.run_sync(question)
+        print(answer.data)
 
